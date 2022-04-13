24ada6bfedc078c8a4aa40b70f1209235c9a3230
rafilek <rvidal@elmi.pl>
2022-04-13 13:05:15 +0200

    Finished section 13. Adding endpoints for Ingredients. All tests passed.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -53,3 +53,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredients_str(self):
+        """Test that the ingredient exists"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Papaya'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
