bbf0e43e65b1eb2863d925337280e2ed5e56a604
rafilek <rvidal@elmi.pl>
2022-04-13 10:40:08 +0200

    Finished section 12. All test passed.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,7 +7,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -21,3 +23,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def perform_create(self, serializer):
         """Create a new ingredient"""
         serializer.save(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """Create tag"""
+        serializer.save(user=self.request.user)     
