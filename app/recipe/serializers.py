2a09b703f0dbc8d399e413678d123bddd526f735
rafilek <rvidal@elmi.pl>
2022-04-20 11:03:53 +0200

    Secction 14. Missing implementation of tests for updating recipes

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -22,11 +22,21 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for the recipe objects"""
+    """Serializer a recipe"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+
+    )
 
     class Meta:
         model = Recipe
-        fields = ('id', 'title', 'tags', 'time_minutes',
+        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
                   'price', 'link')
         read_only_fields = ('id',)
 
